f473d6bb70ad6b6a8c657e454549612d8b945fcf
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-05-18 21:11:35 +0700

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # simple-ampify
+
 A simple plain HTML to AMP converter for personal use
+
+![](https://img.shields.io/npm/v/simple-ampify.svg) ![](https://badgen.net/bundlephobia/minzip/simple-ampify) ![](https://img.shields.io/npm/dt/simple-ampify.svg)
+
+## Install
+
+```bash
+$ yarn add simple-ampify
+# OR
+$ npm i simple-ampify
+```
+
+## Usage
+
+```js
+import ampify from 'simple-ampify';
+
+const result = ampify(htmlString)
+```
+
+## With Options
+
+```js
+import ampify from 'simple-ampify';
+
+const options = {
+	analytics : {
+		id: 'UA-XXXXXX-XX'
+	},
+	adsense : {
+		id: 'XXXXXXXXX'
+	}
+}
+const result = ampify(htmlString, options)
+```
+
+---
+
+Copyright © 2020 Built with ❤️ by Irfan Maulana
---
+++ src/ampify.ts
@@ -0,0 +1,100 @@
+interface IdOption {
+	id?: string;
+}
+
+interface AmpifyOptions {
+	analytics?: IdOption;
+	adsense?: IdOption;
+}
+
+const ampCSSBoilerplate = '<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>';
+
+const getAmpScript = (analytics: boolean, adsense: boolean) => {
+	const baseScript = `<script async src="https://cdn.ampproject.org/v0.js"></script>`;
+	const analyticsScript = `<script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>`;
+	const adsenseScript = `<script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>`;
+
+	let res = baseScript;
+	if (analytics) {
+		res += analyticsScript;
+	}
+	if (adsense) {
+		res += adsenseScript;
+	}
+	return res;
+}
+/**
+ * @param {string} html - a Html string
+ * @param {Object} html - a n options {analytics: {id}, adsense: {id}}
+ *
+ */
+export default (html: string, options?: AmpifyOptions) => {
+	// Add ⚡ to html tag
+	html = html.replace(/<html/gi, '<html ⚡')
+
+	// Combine css into single tag
+	let styleConcat = ''
+	// @ts-ignore
+	html = html.replace(/<style[^>]*data-vue-ssr[^>]*>(.*?)?<\/style>/gi, (match, sub) => {
+		styleConcat += sub
+		return ''
+	})
+	html = html.replace('</head>', `<style amp-custom>${styleConcat}</style></head>`)
+
+	// Remove all !important in CSS
+	html = html.replace(/!important/gi, '')
+
+	// Remove preload and prefetch tags
+	html = html.replace(/<link[^>]*rel="(?:preload|prefetch)?"[^>]*>/gi, '')
+
+	// Remove amphtml tag
+	html = html.replace(/<link[^>]*rel="(?:amphtml)?"[^>]*>/gi, '')
+
+	// Replace img tags with amp-img
+	// @ts-ignore
+	html = html.replace(/<img([^>]*)>/gi, (match, sub) => {
+		return `<amp-img ${sub} layout=responsive></amp-img>`
+	})
+
+	// Remove data attributes from tags
+	html = html.replace(/\s*data-(?:[^=>]*="[^"]*"|[^=>\s]*)/gi, '')
+
+	// Remove JS script tags except for ld+json
+	html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, (match) => {
+		return (/application\/ld\+json/gi.test(match)) ? match : ''
+	})
+
+	// Add AMP script before </head>
+	html = html.replace('</head>', getAmpScript(Boolean(options.analytics), Boolean(options.adsense)) + ampCSSBoilerplate + '</head>')
+
+	// Add AMP analytics
+	if (options.analytics && options.analytics.id) {
+		html = html.replace('</body>',
+			`
+			<amp-analytics type='googleanalytics'>
+			<script type='application/json'>
+				{
+					"vars": {
+						"account": "${options.analytics.id}"
+					},
+					"triggers": {
+						"trackPageview": {
+							"on": "visible",
+							"request": "pageview"
+						}
+					}
+				}
+			</script>
+		</amp-analytics>
+	</body>`)
+	}
+
+	if (options.adsense && options.adsense.id) {
+		html = html.replace('</body>',
+			`
+			<amp-auto-ads type="adsense" data-ad-client="${options.adsense.id}"></amp-auto-ads>
+	</body>`)
+	}
+
+	return html
+}
